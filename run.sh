0994510a60009bd67a2ca5333626d864e61bf743
AllenZYJ <zjvideo@hotmail.com>
2020-05-19 09:12:11 +0800

    A commit a day keeps the girlfriend away.

---
+++ run.sh
@@ -0,0 +1,6 @@
+cd /home/document/autogreen/newone/
+echo $(date)>>CommitTime.txt
+git add .
+git commit -m "A commit a day keeps the girlfriend away."
+git push -u origin master
+echo I'M Stronger..
